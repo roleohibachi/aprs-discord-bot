8eaabe3ed34414059994f222d0f21e754a044916
roleo <roleohibachi@gmail.com>
2023-03-29 15:07:57 -0600

    progress on async and updates to sync

--- aprs-bot-async.py
+++ aprs-bot-async.py
@@ -1,6 +1,7 @@
 import os
 import re
 import time
+from datetime import datetime
 import sys
 import argparse
 import traceback
@@ -10,9 +11,9 @@ import discord
 from discord.ext import commands
 import asyncio
 
-def send_aprs_msg(AIS:  aprslib.IS, fromCall: str, toCall: str, message: str, lineNo: int): 
+async def send_aprs_msg(AIS:  aprslib.IS, fromCall: str, toCall: str, message: str, lineNo: int): 
     message=re.sub(r'[{:]','',message)
-    pkt=fromCall+">APP614"+",TCPIP*::"+toCall.ljust(9, " ")+":"+message.replace('{', '')+"{"+str(lineNo)
+    pkt=fromCall+">APP614"+",TCPIP*::"+toCall.ljust(9, " ")+":"+message+"{"+str(lineNo)
     if logging.getLogger().isEnabledFor(logging.DEBUG):
         logging.info("Simulated send: "+pkt)
     else:
@@ -20,13 +21,18 @@ def send_aprs_msg(AIS:  aprslib.IS, fromCall: str, toCall: str, message: str, li
         logging.info("Sent: "+pkt)
     return lineNo + 1
 
-def send_aprs_ack(AIS: aprslib.IS, toCall: str, msgNo: int, fromCall: str):
+async def send_aprs_ack(AIS: aprslib.IS, toCall: str, msgNo: int, fromCall: str):
     pkt=fromCall+">APP614"+",TCPIP*::"+toCall.ljust(9, " ")+":ack"+str(msgNo)
     if logging.getLogger().isEnabledFor(logging.DEBUG):
         logging.info("[DEBUG] Simulated ACK: "+pkt)
+        await asyncio.sleep(30)
+        logging.info("[DEBUG] Simulated ACK (double-tap): "+pkt)
     else:
+        logging.info("Sending ACK: "+pkt)
         sent=AIS.sendall(pkt)
-        logging.info("Send: "+pkt)
+        await asyncio.sleep(30)
+        logging.info("Sending ACK (double-tap): "+pkt)
+        sent=AIS.sendall(pkt)
 
 class DiscordClient(discord.Client):
     async def on_ready(self):
@@ -34,12 +40,35 @@ class DiscordClient(discord.Client):
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
-        if message.author == client.user:
+        if message.author == myDiscordClient.user:
             return
         if message.content.startswith('$hello'):
             await message.channel.send('Hello!')
 
-if __name__ == "__main__":
+from collections import Mapping
+class TenRingDict(dict):
+    def __init__(self, other=None, **kwargs):
+        super().__init__()
+        self.update(other, **kwargs)
+
+    def __setitem__(self, key, value):
+        if len(self)>=10:
+            self.pop(next(iter(self)))
+        super().__setitem__(key, value)
+
+    def update(self, other=None, **kwargs):
+        if other is not None:
+            for k, v in other.items() if isinstance(other, Mapping) else other:
+                self[k] = v
+        for k, v in kwargs.items():
+            self[k] = v
+
+class APRSClient():
+    def __init__(self, *args, **kwargs):
+            
+async def bridge(DiscordClient,APRSClient):
+
+async def main():
     parser = argparse.ArgumentParser(description='Bridge between APRS and Discord.')
 
     parser.add_argument( '-log',
@@ -62,54 +91,69 @@ if __name__ == "__main__":
     logging.basicConfig( level=args.loglevel.upper() )
     aprsMsgNo=args.aprsMsgNo
 
-    lastHeard = dict()
+    lastHeard = TenRingDict()
 
     #configure Discord
     intents = discord.Intents.default()
     intents.message_content = True
-    client = DiscordClient(intents=intents)
+    myDiscordClient = DiscordClient(intents=intents)
 
     #configure APRS
-    def aprs_handler(packet):
-        try:
-            logging.debug("Received APRS: "+packet)
-            packet = aprslib.parse(packet)
-            if 'format' in packet and packet['format'] == "message":
-                if 'response' in packet and packet['response'] == "ack":
-                    logging.debug("Got an ACK for message "+packet['msgNo'])
-                elif 'message_text' in packet:
-                    logging.debug("Got a message! Here it is: "+packet['from'] + ": " + packet['message_text']+"... msgno "+packet['msgNo'])
-                    if packet['msgNo'] > lastHeard[packet['from']]:
-                        discord_send(args.botChannelID,packet['from'] + ": " + packet['message_text'])
-                        logging.debug("sending ACK.")
-                        send_aprs_ack(AIS,fromCall=args.botCall,toCall=packet['from'],msgNo=packet['msgNo'])
-                        logging.debug("Recording last heard msg number, so we don't retransmit to discord")
-                        lastHeard.update({packet['from']:packet['msgNo']})
-                    else:
-                        logging.debug('Heard this one before - not posting')
-        except (aprslib.ParseError, aprslib.UnknownFormat) as exp:
-            logging.info("Parsing that packet failed - unknown format.")
-
     AIS = aprslib.IS(args.adminCall,args.adminPass,host=args.aprsHost,port=args.aprsPort)
     AIS.set_filter("g/"+args.botCall)
-    AIS.connect()
-    AIS.consumer(aprs_handler, raw=True, blocking=False)
 
     try:
-        loop=asyncio.get_event_loop()
-        discordTask = loop.create_task(client.start(args.botSecret))
-        loop.run_until_complete(client.wait_until_ready())
-        discordChannel = client.get_channel(int(args.botChannelID))
-        loop.run_until_complete(discordChannel.send("I'm online"))
-        loop.run_forever()
+        await myDiscordClient.start(args.botSecret)
+        #await myDiscordClient.wait_until_ready()
+        discordChannel = myDiscordClient.get_channel(int(args.botChannelID))
+
+        await discordChannel.send("I'm online")
+
+        async def aprs_handler(packet):
+            try:
+                packet = aprslib.parse(packet)
+                if 'format' in packet and packet['format'] == "message":
+                    if 'response' in packet and packet['response'] == "ack":
+                        logging.debug("Got an ACK for message "+packet['msgNo'])
+                    elif 'message_text' in packet:
+                        logging.debug("Got a message! Here it is: "+packet['from'] + ": " + packet['message_text']+"... msgno "+packet['msgNo'])
+                        if not packet['from'] in lastHeard:
+                            lastHeard.update({packet['from']:'0'})
+                        if int(packet['msgNo']) > int(lastHeard[packet['from']]):
+                            embed=discord.Embed.from_dict({
+                                        "title": packet['from']+": ",
+                                        "type": "rich",
+                                        "description": packet['message_text'],
+                                        "url": "https://aprs.fi/?c=raw&call="+packet['from'],
+                                        "timestamp": str(datetime.now()),
+                                        "footer": {
+                                            "text": "Licensed radio amateurs can post to this channel by sending APRS messages to callsign 'PPRAA' with standard message format.",
+                                        },
+                                        "fields": [
+                                            {"name": "via", "value": packet['via'], "inline": True},
+                                            {"name": "msgNo", "value": packet['msgNo'], "inline": True},
+                                        ],
+                                    })
+                            #ackTask = loop.create_task(send_aprs_ack(AIS,fromCall=args.botCall,toCall=packet['from'],msgNo=packet['msgNo']))
+                            await send_aprs_ack(AIS,fromCall=args.botCall,toCall=packet['from'],msgNo=packet['msgNo'])
+                            #discordSendTask = loop.create_task(discordChannel.send(embed=embed))
+                            await discordChannel.send(embed=embed)
+                            lastHeard.update({packet['from']:packet['msgNo']})
+                        else:
+                            logging.debug('Heard this one before - not posting, repeating ACK')
+                            #ackTask = loop.create_task(send_aprs_ack(AIS,fromCall=args.botCall,toCall=packet['from'],msgNo=packet['msgNo']))
+                            await send_aprs_ack(AIS,fromCall=args.botCall,toCall=packet['from'],msgNo=packet['msgNo'])
+            except (aprslib.ParseError, aprslib.UnknownFormat) as exp:
+                logging.info("Parsing that packet failed - unknown format.")
+        
+        AIS.connect()
+        await loop.run_in_executor(executor=None,func=AIS.consumer(aprs_handler, raw=True, blocking=True))
+
     except KeyboardInterrupt:
         logging.info("Shutting down gracefully")
         #aprsMsgNo = await send_aprs_msg(AIS,fromCall=args.botCall,toCall=args.adminCall+adminSSID,message="script offline",lineNo=aprsMsgNo)
-        #loop.run_until_complete(discord_send(args.botChannelID,"I'm shutting down"))
-        loop.run_until_complete(discordChannel.send("I'm shutting down"))
-        loop.run_until_complete(client.close())
-        loop.stop()
-        AIS.close()
+        await discordChannel.send("I'm shutting down")
+        await loop.run_in_executor(executor=None,func=AIS.close())
         print('aprsMsgNo on exit: '+str(aprsMsgNo))
 
 
@@ -117,5 +161,6 @@ if __name__ == "__main__":
     #aprsMsgNo = await send_aprs_msg(AIS,fromCall=args.botCall,toCall=args.adminCall+adminSSID,message="bot online",lineNo=aprsMsgNo)
 
     #run the bot
-    #asyncio.run(main())
 
+if __name__ == "__main__":
+    asyncio.run(main())
--- aprs-bot.py
+++ aprs-bot.py
@@ -1,86 +1,122 @@
-import aprslib
 import os
 import re
 import time
+from datetime import datetime
 import sys
+import argparse
 import traceback
+import logging
+import aprslib
 from discordwebhook import Discord
 
-live=True
 
 def send_aprs_msg(AIS:  aprslib.IS, fromCall: str, toCall: str, message: str, lineNo: int): 
-    global live
     message=re.sub(r'[{:]','',message)
-    pkt=fromCall+">APP614"+",TCPIP*::"+toCall.ljust(9, " ")+":"+message.replace('{', '')+"{"+str(lineNo)
-    if(live):
-        sent=AIS.sendall(pkt)
-        print("Send: "+pkt)
+    pkt=fromCall+">APP614"+",TCPIP*::"+toCall.ljust(9, " ")+":"+message+"{"+str(lineNo)
+    if logging.getLogger().isEnabledFor(logging.DEBUG):
+        logging.debug("[DEBUG] Simulated send: "+pkt)
     else:
-        print("[DEBUG] Simulated send: "+pkt)
+        sent=AIS.sendall(pkt)
+        logging.info("Sent: "+pkt)
     return lineNo + 1
 
 def send_aprs_ack(AIS: aprslib.IS, toCall: str, msgNo: int, fromCall: str):
-    global live
     pkt=fromCall+">APP614"+",TCPIP*::"+toCall.ljust(9, " ")+":ack"+str(msgNo)
-    if(live):
-        sent=AIS.sendall(pkt)
-        print("Send: "+pkt)
+    if logging.getLogger().isEnabledFor(logging.DEBUG):
+        logging.debug("[DEBUG] Simulated ACK: "+pkt)
     else:
-        print("[DEBUG] Simulated ACK: "+pkt)
+        sent=AIS.sendall(pkt)
+        #todo: wait 30 and double-tap
+        print("Sent ACK: "+pkt)
 
+from collections import Mapping
+class TenRingDict(dict):
+    def __init__(self, other=None, **kwargs):
+        super().__init__()
+        self.update(other, **kwargs)
+
+    def __setitem__(self, key, value):
+        if len(self)>=10:
+            self.pop(next(iter(self)))
+        super().__setitem__(key, value)
+
+    def update(self, other=None, **kwargs):
+        if other is not None:
+            for k, v in other.items() if isinstance(other, Mapping) else other:
+                self[k] = v
+        for k, v in kwargs.items():
+            self[k] = v
 
 def main():
+    parser = argparse.ArgumentParser(description='Bridge between APRS and Discord.')
+
+    parser.add_argument( '-log',
+        '--loglevel',
+        default='warning',
+        help='Provide logging level. Example --loglevel debug, default=warning' )
+
+    parser.add_argument( '-bot','--botName', default="aprsbot", help='Username for the bot to use in Discord')
+    parser.add_argument( '--botSecret', default=os.environ.get('DISCORD_WEBHOOK_URL'), help='Discord bot secret.')
+    parser.add_argument( '--botCall', default=os.environ.get('DISCORD_BOT_CALL'), help='Callsign for the bot to use on APRS')
+    parser.add_argument( '--botSSID', default=os.environ.get('DISCORD_BOT_SSID'), help='SSID for the bot to use on APRS. Useful for multiple discord channels. Include the leading dash.')
+    parser.add_argument( '--botChannelID', type=int, default=int(os.environ.get('DISCORD_BOT_CHANNEL')), help='Discord channel ID to bridge.')
+    parser.add_argument( '--adminCall', default=os.environ.get('APRS_CALL'), help='Callsign to authenticate with APRS. Under whose license are you transmitting?')
+    parser.add_argument( '--adminSSID', default=os.environ.get('APRS_SSID'), help='SSID for the admin, who will receive APRS status updates.')
+    parser.add_argument( '--adminPass', default=os.environ.get('APRS_PASSWD'), help='Password for the APRS user.')
+    parser.add_argument( '--aprsHost', default="noam.aprs2.net", help='APRS-IS server')
+    parser.add_argument( '--aprsPort', type=int, default=14580, help='APRS-IS port')
+    parser.add_argument( '--aprsMsgNo', type=int, default=int(time.time()/10%(pow(10,2))), help='The initial serialized message number. If unset, will be random.')
+
+    args = parser.parse_args()
+    logging.basicConfig( level=args.loglevel.upper() )
+    aprsMsgNo=args.aprsMsgNo
+
+    lastHeard = TenRingDict()
 
     #configure APRS
-    botCall="PPRAA"
-    adminCall="AD8IS"
-    adminSSID="-10"
-    passwd=os.environ['APRS_PASSWD']
+    AIS = aprslib.IS(args.adminCall,args.adminPass,host=args.aprsHost,port=args.aprsPort)
+    AIS.set_filter("g/"+args.botCall)
     
-    msgcount=int(time.time()%(pow(10,4))) #gotta start somewhere
-    AIS = aprslib.IS(adminCall,passwd,host="noam.aprs2.net",port=14580)
-    AIS.set_filter("g/"+botCall)
-
     #configure Discord
-    discord = Discord(url=os.environ['DISCORD_WEBHOOK_URL'])
+    discord = Discord(url=args.botSecret)
 
     def aprs_handler(packet):
-        print(packet)
-        if packet['format'] == "message":
-            if 'response' in packet:
-                if packet['response'] == "ack":
-                    print("Got an ACK for message "+packet['msgNo'])
+        if 'format' in packet and packet['format'] == "message":
+            if 'response' in packet and packet['response'] == "ack":
+                logging.debug("Got an ACK for message "+packet['msgNo'])
             elif 'message_text' in packet:
-                print("Got a message! Here it is: "+packet['from'] + ": " + packet['message_text']+"... msgno "+packet['msgNo'])
-                discord.post(
-                    username="aprsbot",
-                    embeds=[
-                        {
-                            "author": {
-                                "name": "New APRS Message Received",
+                logging.debug("Got a message! Here it is: "+packet['from'] + ": " + packet['message_text']+"... msgno "+packet['msgNo'])
+                if not packet['from'] in lastHeard:
+                    lastHeard.update({packet['from']:'0'})
+                if int(packet['msgNo']) > int(lastHeard[packet['from']]):
+                    embed={
+                                "title": packet['from']+": ",
+                                "type": "rich",
+                                "description": packet['message_text'],
                                 "url": "https://aprs.fi/?c=raw&call="+packet['from'],
-                            },
-                            "title": packet['from']+": ",
-                            "description": packet['message_text'],
-                            "fields": [
-                                {"name": "via", "value": packet['via'], "inline": True},
-                                {"name": "msgNo", "value": packet['msgNo'], "inline": True},
-                            ],
-                            "footer": {
-                                "text": "Licensed radio amateurs can post to this channel by sending APRS messages to callsign 'PPRAA' with standard message format. Replies in Discord will not be forwarded (yet...).",
-                            },
-                        }
-                    ],
-                )
+                                "timestamp": str(datetime.now()),
+                                "footer": {
+                                    "text": "Licensed radio amateurs can post to this channel by sending APRS messages to callsign 'PPRAA' with standard message format.",
+                                },
+                                "fields": [
+                                    {"name": "via", "value": packet['via'], "inline": True},
+                                    {"name": "msgNo", "value": packet['msgNo'], "inline": True},
+                                ],
+                            }
+    
+                discord.post(username=args.botName,embeds=[embed])
+                send_aprs_ack(AIS,fromCall=args.botCall,toCall=packet['from'],msgNo=packet['msgNo'])
+                lastHeard.update({packet['from']:packet['msgNo']})
 
-
-                print("sending ACK.")
-                send_aprs_ack(AIS,fromCall=botCall,toCall=packet['from'],msgNo=packet['msgNo'])
+            else:
+                logging.debug('Heard this one before - not posting, repeating ACK')
+                send_aprs_ack(AIS,fromCall=args.botCall,toCall=packet['from'],msgNo=packet['msgNo'])
 
     try:
 
         AIS.connect()
-        msgcount = send_aprs_msg(AIS,fromCall=botCall,toCall=adminCall+adminSSID,message="script online",lineNo=msgcount)
+        aprsMsgNo = send_aprs_msg(AIS,fromCall=args.botCall,toCall=args.adminCall+args.adminSSID,message="script online",lineNo=aprsMsgNo)
+        discord.post(content="Bot online! Send an APRS message to PPRAA to have it posted here.",username=args.botName)
 
         # by default `raw` is False, then each line is ran through aprslib.parse()
         AIS.consumer(aprs_handler, raw=False)
@@ -90,7 +126,8 @@ def main():
 
     except KeyboardInterrupt:
         print("Shutdown requested... notifying admin")
-        msgcount = send_aprs_msg(AIS,fromCall=botCall,toCall=adminCall+adminSSID,message="script offline",lineNo=msgcount)
+        aprsMsgNo = send_aprs_msg(AIS,fromCall=args.botCall,toCall=args.adminCall+args.adminSSID,message="script offline",lineNo=aprsMsgNo)
+        discord.post(content="Bot is now offline.",username=args.botName)
         AIS.close()
     except Exception as err:
         AIS.close()
